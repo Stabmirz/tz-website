0eb25003a69c6f23ba8157cad60459d0d994a954
Mirza <amirza.techlaunch.io>
2020-06-23 05:26:20 -0400

    Updates

---
+++ precache-manifest.3490d8bd169d9e73bf517b71198f7641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc081fd1011e034386f3b912e957c51a",
+    "url": "/tz-website/index.html"
+  },
+  {
+    "revision": "99cf94f6bf53585e8067",
+    "url": "/tz-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cf16ff308308e2db3fd6",
+    "url": "/tz-website/static/css/main.c2a1ab79.chunk.css"
+  },
+  {
+    "revision": "99cf94f6bf53585e8067",
+    "url": "/tz-website/static/js/2.3830316a.chunk.js"
+  },
+  {
+    "revision": "f9c775192440335a01159083e6d5d747",
+    "url": "/tz-website/static/js/2.3830316a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf16ff308308e2db3fd6",
+    "url": "/tz-website/static/js/main.59b674a7.chunk.js"
+  },
+  {
+    "revision": "7c8806080631a58fc913",
+    "url": "/tz-website/static/js/runtime-main.35234585.js"
+  }
+]);
