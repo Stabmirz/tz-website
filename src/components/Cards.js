0ab8102171a7470941bd4bd460b2c4716c0b706f
Mirza <amirza.techlaunch.io>
2020-06-23 21:49:41 -0400

    updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,24 +6,24 @@ const Cards = () => {
         <div className="container">
             <div className="cards-section ">
                 <div>
-                    <img src="./images/cc-white.png" alt="credit card"/>
+                    <img src={process.env.PUBLIC_URL + '/images/cc-white.png'} alt="credit card"/>
                     <h1>Use Just One Card</h1>
                     <h1>For all</h1>
                     <h1>Your Spending</h1>
                 </div>
                 <div>
-                    <img src="./images/transfer-white.png" alt="transfer"/>
+                    <img src={process.env.PUBLIC_URL + '/images/transfer-white.png'} alt="transfer"/>
                     <h1>Transfer Funds</h1>
                     <h1>from Cards</h1>
                     <h1>or Accounts</h1>
                 </div>
                 <div>
-                    <img src="./images/send-to-a-friend-white.png" alt="send money"/>
+                    <img src={process.env.PUBLIC_URL + '/images/send-to-a-friend-white.png'} alt="send money"/>
                     <h1>Send Money</h1>
                     <h1>to a Friend</h1>
                 </div>
                 <div>
-                    <img src="./images/refer-a-friend-white.png" alt="refer a friend"/>
+                    <img src={process.env.PUBLIC_URL + '/images/refer-a-friend-white.png'} alt="refer a friend"/>
                     <h1>Refer A Friend</h1>
                     <h1>earn Rewards</h1>
                 </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return ( 
-        <div className="footer container">
-            <h1>Footer</h1>
+  return (
+    <div className="footer container">
+      <div className="footer-section">
+        <div className="footer-logo">
+          <img src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo" />
         </div>
-    );
-}
- 
+        <div className="footer-links">
+          <div>
+            <div className="pages">
+              <a href="/#waitlist">Waitlist</a>
+            </div>
+            <div className="pages">
+              <a href="/#">Press Inquiries</a>
+            </div>
+            <div className="pages">
+              <a href="/#">Terms of Use</a>
+            </div>
+            <div className="pages">
+              <a href="/#">Privacy Policy</a>
+            </div>
+          </div>
+          <div>
+            <div className="social-media">
+              <a href="/#">Facebook</a>
+            </div>
+            <div className="social-media">
+              <a href="/#">Twitter</a>
+            </div>
+            <div className="social-media">
+              <a href="/#">Instagram</a>
+            </div>
+            <div className="social-media">
+              <a href="/#">LinkedIn</a>
+            </div>
+            <div className="social-media">
+              <a href="/#">YouTube</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-note">
+        <span>© 2020 TranZsend LLC. All right reserved.</span>
+        <br />
+        <p>
+          The TranZsend card is issued by WebBank, Member FDIC, pursuant to a
+          license from Mastercard International. TranZsend Cheeking is provided
+          through Evolve Bank & Trust, Member FDIC. All text, graphics, audio
+          files, code downloadable material, and other works on this website are
+          the copyrighted works of Zero Financial, Inc. All right Reserved. Any
+          unauthorized redistribution or reproduction of any copyrighted
+          materials on this website is strictly prohibited. Other product and
+          company names are trademarks of their respective owners. This website
+          contains simulated images; actual appearance may vary.
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
