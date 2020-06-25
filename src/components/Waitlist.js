b3a8d06bb7efdaecabe39077103030a530c65dd4
Mirza <amirza.techlaunch.io>
2020-06-25 13:47:11 -0400

    mailchimp form added successfully

--- src/components/Waitlist.js
+++ src/components/Waitlist.js
@@ -10,21 +10,24 @@ const Waitlist = () => {
             <div className="join-waitlist animate__animated animate__fadeInUp">
                 <h5>Be one of the first to access TranZsend.</h5>
                 <br/>
-{/* 
-                <form action="https://tranzsend.us10.list-manage.com/subscribe/post?u=24131087bfbc7de64dd678000&amp;id=c05dd4ee89" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                    <div id="mc_embed_signup_scroll">
-                    
-                        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
-                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                        <input type="text" name="b_24131087bfbc7de64dd678000_c05dd4ee89" value=""/>
-                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
-                    </div>
-                </form> */}
 
-                <form action="https://tranzsend.us10.list-manage.com/subscribe/post?u=24131087bfbc7de64dd678000&amp;id=c05dd4ee89" method="post" className="animate__animated animate__fadeInUp" target="_blank" noValidate>
-                    <input type="text" className="border" placeholder="name@email.com"/>
+                {/* <!-- Begin Mailchimp Signup Form --> */}
+
+                <div id="mc_embed_signup">
+                    <form action="https://tranzsend.us10.list-manage.com/subscribe/post?u=24131087bfbc7de64dd678000&amp;id=c05dd4ee89" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" noValidate>
+                        <div id="mc_embed_signup_scroll">
+                            <input type="email" name="EMAIL" className="border" id="mce-EMAIL" placeholder="name@email.com" required/>
+                            <input type="submit" value="Join Waitlist" name="subscribe" id="mc-embedded-subscribe" className="border"/>
+                        </div>
+                    </form>
+                </div>
+
+                {/* <!--End mc_embed_signup--> */}
+
+                {/* <form action="https://tranzsend.us10.list-manage.com/subscribe/post?u=24131087bfbc7de64dd678000&amp;id=c05dd4ee89" method="post" className="animate__animated animate__fadeInUp" target="_blank" noValidate>
+                    <input type="text" className="border" name="b_24131087bfbc7de64dd678000_c05dd4ee89" placeholder="name@email.com"/>
                     <input type="submit" className="border" value="Join Waitlist"/>
-                </form>
+                </form> */}
             </div>
         </div>
     )
