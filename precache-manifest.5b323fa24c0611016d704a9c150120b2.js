d89b22c972b6b5e83310a18f35b7fc788bc51a61
Mirza <amirza.techlaunch.io>
2020-06-25 01:11:26 -0400

    Updates

---
+++ precache-manifest.5b323fa24c0611016d704a9c150120b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7e9dbab34bdfac3e1fefc71050a6db",
+    "url": "/tz-website/index.html"
+  },
+  {
+    "revision": "410e24a2df9dc4fd67f3",
+    "url": "/tz-website/static/css/2.37620a9f.chunk.css"
+  },
+  {
+    "revision": "e7520894f38b2abf69ca",
+    "url": "/tz-website/static/css/main.7bba3dd1.chunk.css"
+  },
+  {
+    "revision": "410e24a2df9dc4fd67f3",
+    "url": "/tz-website/static/js/2.10ddaaed.chunk.js"
+  },
+  {
+    "revision": "f9c775192440335a01159083e6d5d747",
+    "url": "/tz-website/static/js/2.10ddaaed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7520894f38b2abf69ca",
+    "url": "/tz-website/static/js/main.9ff5d6f4.chunk.js"
+  },
+  {
+    "revision": "7c8806080631a58fc913",
+    "url": "/tz-website/static/js/runtime-main.35234585.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tz-website/precache-manifest.4264d3d7ceafbca78edbff7a4d41031d.js"
+  "/tz-website/precache-manifest.5b323fa24c0611016d704a9c150120b2.js"
 );
 
 self.addEventListener('message', (event) => {
